9bf8c99ec85615ef4b1178de6b2ed710de2a25ef
SenArnab <senarnab414@gmail.com>
2025-02-07 22:54:54 +0530

    commit 02/07

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const card = () => {
+    return (
+        <div>
+            <h2>Card Component</h2>
+        </div>
+    )
+}
+
+const App = () => {
+ return (
+     <div>
+      <h2>Functional arrow componet</h2>
+
+     </div>
+ )
+}
+
+
+export default App
