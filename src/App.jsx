87350174ec22c17721fa6c9473e428ba87278d5f
SenArnab <senarnab414@gmail.com>
2025-02-08 13:12:58 +0530

    2/8 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-const card = () => {
-    return (
-        <div>
-            <h2>Card Component</h2>
-        </div>
-    )
-}
+import './App.css'
+import Search from "./components/Search.jsx";
+import {useState} from "react";
+
 
 const App = () => {
- return (
-     <div>
-      <h2>Functional arrow componet</h2>
 
-     </div>
+    const [searchTerm, setSearchTerm] = useState('');
+ return (
+     <main>
+         <div className="pattern"/>
+
+         <div className="wrapper">
+             <header>
+                 <img src="./hero.png" alt="Hero Banner" />
+                 <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+             </header>
+
+             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+         </div>
+     </main>
  )
 }
-
 
 export default App
