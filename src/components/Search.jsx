1f0da9a67e7c7522b5749f6b33e1f364f0dc5255
SenArnab <senarnab414@gmail.com>
2025-02-09 20:37:11 +0530

    card, appwrite and others

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,7 @@ const Search = ({searchTerm, setSearchTerm}) => {
 
             <input
                 type="text"
-                placeholder="Search Through countless movies"
+                placeholder="Search Through Countless Movies"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
